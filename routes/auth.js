86f2a69b81e555db150e3ab8fd7d9c9eb5aca3a7
dilmurod khamidov <dilmuroddelitot@gmail.com>
2023-04-20 11:06:34 +0500

    auth router

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,7 @@ router.get("/login", (req, res) => {
     isLogin: true,
     loginError: req.flash("loginError"),
   });
-});
+})
 
 router.get("/register", (req, res) => {
   if(req.cookies.token) {
