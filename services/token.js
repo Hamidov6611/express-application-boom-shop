86f95e8199c2b92228adb610abab82798490decc
dilmurod khamidov <dilmuroddelitot@gmail.com>
2023-04-26 23:07:48 +0500

    generete tooken

--- services/token.js
+++ services/token.js
@@ -1,8 +1,10 @@
 import jwt from "jsonwebtoken";
 
-const genereteToken = userId => {
-    const accessToken = jwt.sign({userId},process.env.JWT_SECRET, {expiresIn: '30d'})
-    return accessToken
-}
+const genereteToken = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  return accessToken;
+};
 
-export {genereteToken};
+export { genereteToken };
